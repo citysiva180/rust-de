975c2d433ef8322ccac9517c5ac9a03de0ea93d2
Sivarajan <citysiva180@users.noreply.github.com>
2023-08-31 07:48:42 +0530

    RUST LEARNING | Reading Input from User

--- new_prog.rs
+++ new_prog.rs
@@ -2,7 +2,23 @@
 // to run the script the file needs to be made as executable 
 // first run rustc file_name.rs 
 // then run ./filename without any extension to run the code 
+// If you make changes to your file, you need to re-compile it with 
+// rustc and then run the code to find the results!
+
+
+// fn main() {
+//     println!("Hello World!");
+// }
+
+use std::io;
 
 fn main() {
-    println!("Hello World!");
+    println!("What is your name?");
+
+    let mut name = String::new();
+
+    io::stdin().read_line(&mut name)
+        .expect("Failed to read input.");
+
+    println!("Hello, {}! Welcome to the world of Rust!", name.trim());
 }
