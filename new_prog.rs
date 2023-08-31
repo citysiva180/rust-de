20bfd97c2317909c36853e3a0d15287d80ac09ae
Sivarajan <citysiva180@users.noreply.github.com>
2023-08-31 07:45:54 +0530

    RUST LEARNING | Initial Program

---
+++ new_prog.rs
@@ -0,0 +1,8 @@
+// First rust program for data engineering 
+// to run the script the file needs to be made as executable 
+// first run rustc file_name.rs 
+// then run ./filename without any extension to run the code 
+
+fn main() {
+    println!("Hello World!");
+}
